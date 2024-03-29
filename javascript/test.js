6e94e4301c97b0e7aaac405d81439676f51710d0
vanh <caovietanhhd@gmail.com>
2024-03-29 13:48:56 +0700

    front-end

---
+++ javascript/test.js
@@ -0,0 +1,20 @@
+alert("yellow")
+console.log("hello world from vah")
+
+let c = 5
+c = 7
+console.log("test string",c)
+
+var b = 7
+var a = b + c
+console.log(a)
+let e = "hello"
+console.log(typeof(e))
+
+
+function sum(s1,s2) {
+    return s1+s2
+}
+let s1 = 10,s2 = 20 ;
+console.log(sum(s1,s2))
+
